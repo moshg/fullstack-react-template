112a42dbcd1f47acee96cc725c6439295280dcc6
mosh <mosh.ohayo@gmail.com>
2025-03-07 22:50:42 +0900

    feat: Add drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+	out: "./drizzle",
+	schema: "./app/db/schema.ts",
+	dialect: "sqlite",
+	dbCredentials: {
+		// biome-ignore lint/style/noNonNullAssertion: This is a config file, strict type handling is not necessary
+		url: process.env.DB_FILE_NAME!,
+	},
+});
