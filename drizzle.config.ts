5085708e86231ff877aef71ba2ccc50847acf393
mosh <mosh.ohayo@gmail.com>
2025-03-17 21:22:04 +0900

    chore: generate initial db
    pnpm db:generate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
 	out: "./drizzle",
-	schema: "./app/db/schema.ts",
+	schema: "./app/server/db/schema.ts",
 	dialect: "sqlite",
 	dbCredentials: {
 		// biome-ignore lint/style/noNonNullAssertion: This is a config file, strict type handling is not necessary
